d4f24c36f2bf84b1457e41c612b25ab2ebc3d092
Mario Camelo <mariuo@gmail.com>
2022-03-04 08:42:43 -0500

    Backend project created

---
+++ backend/src/main/java/com/mcamelo/simiosHuman/SimiosHumanApplication.java
@@ -0,0 +1,13 @@
+package com.mcamelo.simiosHuman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimiosHumanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimiosHumanApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mcamelo/simiosHuman/SimiosHumanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcamelo.simiosHuman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimiosHumanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
