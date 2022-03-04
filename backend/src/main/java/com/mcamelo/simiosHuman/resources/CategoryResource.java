888f2413037a09878bc09a01dae894f1fb55119a
Mario Camelo <mariuo@gmail.com>
2022-03-04 09:58:03 -0500

    Created category findById

--- backend/src/main/java/com/mcamelo/simiosHuman/resources/CategoryResource.java
+++ backend/src/main/java/com/mcamelo/simiosHuman/resources/CategoryResource.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mcamelo.simiosHuman.entities.Category;
+import com.mcamelo.simiosHuman.dtos.CategoryDTO;
 import com.mcamelo.simiosHuman.services.CategoryService;
 
 @RestController
@@ -19,9 +20,15 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+	public ResponseEntity<List<CategoryDTO>> findAll(){
+		List<CategoryDTO> listDto = service.findAll();
+				
+		return ResponseEntity.ok().body(listDto);
+	}
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+		CategoryDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 
 }
--- backend/src/main/java/com/mcamelo/simiosHuman/services/CategoryService.java
+++ backend/src/main/java/com/mcamelo/simiosHuman/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.mcamelo.simiosHuman.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.mcamelo.simiosHuman.dtos.CategoryDTO;
 import com.mcamelo.simiosHuman.entities.Category;
 import com.mcamelo.simiosHuman.repositories.CategoryRepository;
 
@@ -16,9 +18,17 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();		
 		
-		return repository.findAll();		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Category entity = repository.findById(id).get();
+		CategoryDTO dto = new CategoryDTO(entity);
+		return dto;
 	}
 	
 
